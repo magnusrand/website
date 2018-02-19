9eb6342585d23755a252deac2b048ec05647f89a
Magnus Rand <magnus.rand@gmail.com>
2018-02-19 14:25:34 +0100

    En del endringer i forbindelse med søknad

--- paranden/js/init.js
+++ paranden/js/init.js
@@ -5,11 +5,13 @@
     $('.parallax').parallax();
     $('.carousel').carousel({duration:200});
 	$(".dropdown-button").dropdown();
-    
+
   }); // end of document ready
    // Initialize collapse button
   $(".button-collapse").sideNav();
   $('.carousel').carousel();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
+
+
 })(jQuery); // end of jQuery name space
