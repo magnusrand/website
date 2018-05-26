0aefd234e4d468c2e1c116d72fa5f5018d0ff74f
Magnus Rand <magnus.rand@gmail.com>
2018-05-27 00:53:20 +0200

    Lagt til smoothscroll

--- resources/js/main.js
+++ resources/js/main.js
@@ -14,3 +14,17 @@ $(window).scroll(function() {
       $('#down-arrow').fadeIn();
      }
  });
+
+
+document.getElementById("navbtn1").addEventListener("click", function(){ smoothscroll("section2");});
+document.getElementById("navbtn2").addEventListener("click", function(){ smoothscroll("section3");});
+document.getElementById("navbtn3").addEventListener("click", function(){ smoothscroll("section4");});
+
+document.getElementById("down-arrow").addEventListener("click", function(){ smoothscroll("section2");});
+
+// Scroll to a certain element
+function smoothscroll(element) {
+    document.getElementById(element).scrollIntoView({
+        behavior: 'smooth',
+        block: "start"
+});}
