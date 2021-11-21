1e99e09416143fbae661e28048c5b754b7580afa
Magnus Rand <magnus.rand@gmail.com>
2021-11-21 11:50:42 +0100

    Move to react

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Velkommen
-Dette repoet inneholder all kode brukt i nettsiden min **magnusrand.com**. Ikke alt er *skapt* av meg, dvs. noe er kodet fra tutorials på nettsteder som YouTube. 
+
+Dette repoet inneholder all kode brukt i nettsiden min **magnusrand.com**. Ikke alt er _skapt_ av meg, dvs. noe er kodet fra tutorials på nettsteder som YouTube.
+
+# empty-project
+
+Empty project.
+
+## Building and running on localhost
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+To create a production build:
+
+```sh
+npm run build-prod
+```
+
+To create a development build:
+
+```sh
+npm run build-dev
+```
+
+## Running
+
+Open the file `dist/index.html` in your browser
+
+## Credits
+
+Made with [createapp.dev](https://createapp.dev/)
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+// const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const OUTPUT_PATH = path.resolve(__dirname, 'dist')
+
+const config = (env, args) => ({
+    mode: args.mode === 'production' ? 'production' : 'development',
+    entry: './src/index.tsx',
+    output: {
+        path: OUTPUT_PATH,
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x)?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: { presets: ['@babel/preset-env'] },
+                },
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            templateContent: ({ htmlWebpackPlugin }) =>
+                '<!DOCTYPE html><html><head><meta charset="utf-8"><title>' +
+                htmlWebpackPlugin.options.title +
+                '</title></head><body><div id="app"></div></body></html>',
+            filename: 'index.html',
+        }),
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', 'jsx'],
+    },
+})
+
+module.exports = config
