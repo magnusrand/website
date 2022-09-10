86efe730f72b293502a5f379815fa28f1b110254
Magnus Rand <magnus.rand@gmail.com>
2022-09-11 01:24:55 +0200

    add fullscreen view mode for images

--- src/pages/Photography/DisplayPhotosPage.tsx
+++ src/pages/Photography/DisplayPhotosPage.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
-// import { wrapGrid } from 'animate-css-grid'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom'
+
+// import { wrapGrid } from 'animate-css-grid'
+import classNames from 'classnames'
 
 import { HomeLogo } from '../../components/NavBar/HomeLogo'
 import { TextDivider } from '../../components/TextDivider'
@@ -9,72 +11,28 @@ import { PhotoData } from '../../types'
 import MainNavBar from '../MainNavBar'
 
 export const DisplayPhotosPage = () => {
-    const [photoLinks, setPhotoLinks] = useState<PhotoData[]>([])
-    const [gridStyle, setGridStyle] = useState<number>(1)
-    const [photoLayout, setPhotoLayout] = useState<number[]>([])
     const gridRef = useRef<HTMLDivElement>(null)
     const params = useParams()
+    const [photoLinks, setPhotoLinks] = useState<PhotoData[]>([])
+    const [gridStyle] = useState<number>(1)
+    const [photoLayout, setPhotoLayout] = useState<number[]>([])
+    const [currentFullscreen, setCurrentFullscreen] = useState('')
 
     const albumName = params.albumName?.toLowerCase()
 
-    // useLayoutEffect(() => {
-    //     if (gridRef.current)
-    //         wrapGrid(gridRef.current, {
-    //             easing: 'easeInOut',
-    //             stagger: 10,
-    //             duration: 400,
-    //         })
-    // }, [gridRef])
+    /** PhotoGrid experimental
+    useLayoutEffect(() => {
+        if (gridRef.current)
+            wrapGrid(gridRef.current, {
+                easing: 'easeInOut',
+                stagger: 10,
+                duration: 400,
+            })
+    }, [gridRef])
 
-    // const handleChangePhotoGrid = () => {
-    //     console.log('change photo grid')
-    //     setGridStyle((gridStyle % 2) + 1)
-    // }
-
-    useEffect(() => {
-        const getPhotosLink = async () => {
-            const photoLinkData = await getPhotosInAlbum(albumName)
-            setPhotoLinks(photoLinkData ?? [])
-        }
-        getPhotosLink()
-    }, [albumName])
-
-    useEffect(() => {
-        switch (gridStyle) {
-            case 1:
-                setPhotoLayout(photoGrid1(photoLinks))
-                break
-            case 2:
-                setPhotoLayout(photoGrid2(photoLinks))
-                break
-            default:
-                setPhotoLayout(photoGrid1(photoLinks))
-        }
-    }, [photoLinks, gridStyle])
-
-    const photoGrid1 = (__photoLinks: PhotoData[]) => {
-        const layoutArray = []
-        let counter = 0
-        while (counter < __photoLinks?.length) {
-            if (counter + 1 === __photoLinks?.length) {
-                layoutArray.push(1)
-                counter++
-            } else if (
-                __photoLinks[counter].meta?.orientation === 'landscape'
-            ) {
-                layoutArray.push(1)
-                counter++
-            } else if (
-                __photoLinks[counter + 1].meta?.orientation === 'portrait'
-            ) {
-                layoutArray.push(2, 3)
-                counter += 2
-            } else {
-                layoutArray.push(11)
-                counter++
-            }
-        }
-        return layoutArray
+    const handleChangePhotoGrid = () => {
+        console.log('change photo grid')
+        setGridStyle((gridStyle % 2) + 1)
     }
 
     const photoGrid2 = (__photoLinks: PhotoData[]) => {
@@ -90,6 +48,68 @@ export const DisplayPhotosPage = () => {
             }
         }
         return layoutArray
+    }*/
+
+    useEffect(() => {
+        const getPhotosLink = async () => {
+            const photoLinkData = await getPhotosInAlbum(albumName)
+            setPhotoLinks(photoLinkData ?? [])
+        }
+        getPhotosLink()
+    }, [albumName])
+
+    const photoGrid1 = useMemo(() => {
+        const layoutArray = []
+        let dividerArray = new Array(photoLinks.length)
+            .fill(1)
+            .map(() => (Math.random() >= 0.8 ? 1 : 0))
+        let counter = 0
+        while (counter < photoLinks?.length) {
+            if (counter + 1 === photoLinks?.length) {
+                layoutArray.push(1)
+                counter++
+            } else if (photoLinks[counter].meta?.orientation === 'landscape') {
+                layoutArray.push(1)
+                counter++
+            } else if (
+                photoLinks[counter + 1].meta?.orientation === 'portrait'
+            ) {
+                if (dividerArray[counter] === 1) {
+                    dividerArray = dividerArray.map((value, index) =>
+                        index === counter ? 0 : value,
+                    )
+                }
+                layoutArray.push(2, 3)
+                counter += 2
+            } else {
+                layoutArray.push(11)
+                counter++
+            }
+        }
+        return { layoutArray, dividerArray }
+    }, [photoLinks])
+
+    useEffect(() => {
+        switch (gridStyle) {
+            case 1:
+                setPhotoLayout(photoGrid1.layoutArray)
+                break
+            // case 2:
+            //     setPhotoLayout(photoGrid2(photoLinks))
+            //     break
+            default:
+                setPhotoLayout(photoGrid1.layoutArray)
+        }
+    }, [photoLinks, gridStyle, photoGrid1])
+
+    const handleClickImage = (imageId: string) => {
+        if (imageId !== '') {
+            setCurrentFullscreen(imageId)
+            document.body.style.overflow = 'hidden' // remove scrolling
+        } else {
+            setCurrentFullscreen('')
+            document.body.style.overflow = 'visible' // activate scrolling}
+        }
     }
 
     const displayedAlbumName = () => {
@@ -114,17 +134,17 @@ export const DisplayPhotosPage = () => {
                 {`– ${displayedAlbumName()}`}
             </div>
             <div className="divider-box" />
-            {/* <button
-                className="photo-element--1"
-                onClick={handleChangePhotoGrid}
-            >
-                klikk
-            </button> */}
             {photoLinks?.map((photo, index) => (
                 <>
                     <div
                         key={photo.link + 'div'}
-                        className={`photo-element photo-element--${photoLayout[index]} test`}
+                        className={`photo-element photo-element--${
+                            photoLayout[index]
+                        } test ${
+                            currentFullscreen === photo.link
+                                ? 'photo-element--active'
+                                : ''
+                        }`}
                         style={{
                             backgroundImage: `url(${photo.link + '=w1500'})`,
                             backgroundSize: 'cover',
@@ -135,17 +155,31 @@ export const DisplayPhotosPage = () => {
                             key={photo.link + 'img'}
                             className="photo-element__image"
                             src={photo.link + '=w1500'}
-                            style={{ visibility: 'hidden' }}
+                            tabIndex={currentFullscreen ? -1 : 0}
+                            onClick={() => handleClickImage(photo.link)}
                         />
                     </div>
-                    {[1, 11, 3].includes(photoLayout[index]) &&
-                        Math.random() * 8 > 7 && (
-                            <TextDivider
-                                key={photo.link + 'textDivider'}
-                                index={index}
-                                text={displayedAlbumName()}
-                            />
-                        )}
+                    <div
+                        className={classNames('fullscreen__overlay', {
+                            'fullscreen__overlay--active':
+                                currentFullscreen === photo.link,
+                        })}
+                        onClick={() => handleClickImage('')}
+                    >
+                        <img
+                            key={photo.link + 'img'}
+                            className="photo-element__image__fullscreen"
+                            src={photo.link + '=w1500'}
+                            tabIndex={currentFullscreen ? 0 : -1}
+                        />
+                    </div>
+                    {photoGrid1.dividerArray[index] === 1 && (
+                        <TextDivider
+                            key={photo.link + 'textDivider'}
+                            index={index}
+                            text={displayedAlbumName()}
+                        />
+                    )}
                 </>
             ))}
         </div>
