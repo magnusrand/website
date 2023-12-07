5999ee6865264750b60925db22baf6dd9296c411
Magnus Rand <magnus.rand@gmail.com>
2023-12-07 20:51:55 +0100

    add automatic thumbnail creation on photo upload

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,7 @@ import { getFirestore } from 'firebase-admin/firestore'
 import { log } from 'firebase-functions/logger'
 
 import * as exifr from 'exifr'
+import * as sharp from 'sharp'
 
 import { META_DATA_FIELDS } from './utils'
 
@@ -12,25 +13,33 @@ initializeApp()
 
 const ALBUM_COLLECTION = 'albums'
 const PHOTOS_COLLECTION = 'photos'
+const THUMBNAILS_FOLDER = 'thumbnails'
+const THUMBNAIL_PREFIX = 'thumb_'
+const THUMBNAIL_HEIGHT = 100
 
 export const createDocumentForUploadedPhotoInAlbum = storage
     .object()
     .onFinalize(async (object) => {
         const filePath = object.name as string
+        const splitFilePath = filePath?.split('/')
         const fileName = filePath?.includes('/')
-            ? filePath?.split('/')[1]
+            ? splitFilePath[splitFilePath.length - 1]
             : filePath
         const fileBucket = object.bucket
 
-        const albumName = filePath?.includes('/')
-            ? filePath?.split('/')[0]
-            : 'none'
+        const albumName = filePath?.includes('/') ? splitFilePath[0] : 'none'
 
         log('filePath is', filePath)
 
         // Check if the uploaded file is an image
         if (!filePath?.toLowerCase().match(/\.(jpg|jpeg|png|gif)$/i)) {
-            log('File is not an image')
+            log('File is not an image. Gracefully exiting document creation.')
+            return null
+        }
+
+        // check if the uploaded file is a thumbnail
+        if (fileName?.startsWith(THUMBNAIL_PREFIX)) {
+            log('File is a thumbnail. Gracefully exiting document createion.')
             return null
         }
 
@@ -47,10 +56,29 @@ export const createDocumentForUploadedPhotoInAlbum = storage
                 return {}
             })
 
+        // Generate a thumbnail
+        const thumbnailBuffer = await sharp(_file)
+            .resize({
+                height: THUMBNAIL_HEIGHT,
+                withoutEnlargement: true,
+            })
+            .toBuffer()
+
+        const thumbnailFileName = `${THUMBNAIL_PREFIX}${fileName}`
+        const thumbnailFilePath = `${albumName}/${THUMBNAILS_FOLDER}/${thumbnailFileName}`
+
+        await bucket.file(thumbnailFilePath).save(thumbnailBuffer)
+        log('Thumbnail created and uploaded!')
+
+        const _thumbnailFileBucket = bucket.file(thumbnailFilePath)
+        const _thumbnailPublicUrl = await _thumbnailFileBucket.publicUrl()
+        log('Thumbnail url added to document')
+
         const newPhotoData = {
             title: '',
             description: '',
             imageUrl: publicUrl,
+            thumbnailUrl: _thumbnailPublicUrl,
             fileName: fileName,
             metaData: meta,
         }
@@ -75,7 +103,14 @@ export const createDocumentForUploadedPhotoInAlbum = storage
                 numberOfPhotos: numberOfPhotosInAlbum,
             })
 
-            log('Album', albumName, 'updated with URL:', publicUrl)
+            log(
+                'Album',
+                albumName,
+                'updated with photo:',
+                fileName,
+                'and URL:',
+                publicUrl,
+            )
         } else {
             // Create a new document with the image
             const albumRef = getFirestore().collection(ALBUM_COLLECTION).doc()
@@ -84,7 +119,14 @@ export const createDocumentForUploadedPhotoInAlbum = storage
             const photoRef = albumRef.collection(PHOTOS_COLLECTION).doc()
             await photoRef.set(newPhotoData)
 
-            log('Album', albumName, 'created with URL:', publicUrl)
+            log(
+                'Album',
+                albumName,
+                'created with photo:',
+                fileName,
+                'and URL:',
+                publicUrl,
+            )
         }
 
         return null
@@ -102,7 +144,13 @@ export const removeDocumentsForDeletedPhoto = storage
             : 'none'
 
         if (albumName === 'none') {
-            log('Photo not part of an album! Exiting delete process.')
+            log('Photo not part of an album! Gracefull exiting delete process.')
+            return null
+        }
+
+        // check if the deleted file is a thumbnail
+        if (fileName?.startsWith(THUMBNAIL_PREFIX)) {
+            log('File is a thumbnail. Gracefully exiting delete process.')
             return null
         }
 
