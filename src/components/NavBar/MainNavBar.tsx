f13c925a4b83ddaef9e27c68874055b6917231b5
Magnus Rand <magnus.rand@gmail.com>
2024-01-14 14:07:12 +0100

    wip: navbar accessible after scroll

--- src/components/NavBar/MainNavBar.tsx
+++ src/components/NavBar/MainNavBar.tsx
@@ -4,27 +4,66 @@ import { Color } from '../../types'
 
 import { NavBar, NavItem, Dropdown, DropdownItem } from './NavBar'
 
-export const MainNavBar = (): JSX.Element => (
-    <NavBar>
-        <NavItem title="Fotografi" color={Color.DARK1}>
-            <Dropdown>
-                <DropdownItem title="Utvalgte" linkPath="foto/utvalgte" />
-                <DropdownItem title="Alle album" linkPath="foto" />
-            </Dropdown>
-        </NavItem>
-        <NavItem
-            title="GitHub"
-            color={Color.DARK2}
-            expandIcon={false}
-            linkPath="/github"
-        />
-        <NavItem title="Annet" color={Color.DARK3}>
-            <Dropdown>
-                <DropdownItem title="CV" linkPath="" />
-                <DropdownItem title="Kontakt meg" linkPath="" />
-            </Dropdown>
-        </NavItem>
-    </NavBar>
-)
+import './mainNavBar.css'
+
+export const MainNavBar = (): JSX.Element => {
+    React.useEffect(() => {
+        // const handleScroll = () => {
+        // const navBar = document.getElementById('navbar')
+        // if (navBar) {
+        //     if (mainGrid && mainGrid.scrollTop > 0) {
+        //         navBar.classList.add('navbar--scrolled')
+        //     } else {
+        //         navBar.classList.remove('navbar--scrolled')
+        //     }
+        // }
+        // }
+        const mainGrid = document.querySelector('.main-grid')
+        console.log('mainGrid', mainGrid)
+
+        if (mainGrid) {
+            mainGrid.addEventListener('scroll', (event) => {
+                const navBar = document.querySelector('.navbar')
+                if (navBar) {
+                    if (mainGrid && mainGrid.scrollTop > 0) {
+                        navBar.classList.add('navbar--scrolled')
+                    } else {
+                        navBar.classList.remove('navbar--scrolled')
+                    }
+                }
+            })
+        }
+        return () => {
+            if (mainGrid) {
+                mainGrid.removeEventListener('scroll', (event) =>
+                    console.log('scroll fired'),
+                )
+            }
+        }
+    }, [])
+
+    return (
+        <NavBar>
+            <NavItem title="Fotografi" color={Color.DARK1}>
+                <Dropdown>
+                    <DropdownItem title="Utvalgte" linkPath="foto/utvalgte" />
+                    <DropdownItem title="Alle album" linkPath="foto" />
+                </Dropdown>
+            </NavItem>
+            <NavItem
+                title="GitHub"
+                color={Color.DARK2}
+                expandIcon={false}
+                linkPath="/github"
+            />
+            <NavItem title="Annet" color={Color.DARK3}>
+                <Dropdown>
+                    <DropdownItem title="CV" linkPath="" />
+                    <DropdownItem title="Kontakt meg" linkPath="" />
+                </Dropdown>
+            </NavItem>
+        </NavBar>
+    )
+}
 
 export default MainNavBar
