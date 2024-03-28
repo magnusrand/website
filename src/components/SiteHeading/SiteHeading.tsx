02a872a72e86b89d5f6f22e2134568905c983c8d
Magnus Rand <magnus.rand@gmail.com>
2024-03-28 17:57:44 +0100

    Format site heading title

--- src/components/SiteHeading/SiteHeading.tsx
+++ src/components/SiteHeading/SiteHeading.tsx
@@ -2,15 +2,22 @@ import React from 'react'
 
 import './siteHeading.css'
 
-export const SiteHeading = (props: { siteName: string }) => (
-    <>
-        <div className="site-heading__horizontal-bar-top" />
-        <h1 className="site-heading__horizontal-bar-top__text type-garamond-bold font-size-extralarge">
-            Fotografi
-        </h1>
-        <div className="site-heading__horizontal-bar-bottom" />
-        <h2 className="site-heading__horizontal-bar-bottom__text type-garamond-regular font-size-extralarge">
-            {`– ${props.siteName}`}
-        </h2>
-    </>
-)
+export const SiteHeading = (props: { siteName: string }) => {
+    const formatedSiteName = props.siteName
+        .replace(/-/g, ' ')
+        .replace(/\b\w/g, (l) => l.toUpperCase())
+    return (
+        <>
+            <div className="site-heading__horizontal-bar-top">
+                <h1 className="site-heading__horizontal-bar-top__text type-garamond-bold font-size-extralarge">
+                    Fotografi
+                </h1>
+            </div>
+            <div className="site-heading__horizontal-bar-bottom">
+                <h2 className="site-heading__horizontal-bar-bottom__text type-garamond-regular font-size-extralarge">
+                    {`– ${formatedSiteName}`}
+                </h2>
+            </div>
+        </>
+    )
+}
