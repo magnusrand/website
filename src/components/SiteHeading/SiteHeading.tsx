ed56b2fef3e8425f0204e9b84c624f78da7d7527
Magnus Rand <magnus.rand@gmail.com>
2025-03-06 20:56:11 +0100

    feat(site heading): add dynamic font size for heading

--- src/components/SiteHeading/SiteHeading.tsx
+++ src/components/SiteHeading/SiteHeading.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import './siteHeading.css'
 
@@ -6,9 +6,52 @@ export const SiteHeading = (props: {
     siteName: string
     headingRef?: React.RefObject<HTMLDivElement>
 }) => {
+    const MAX_FONT_SIZE = 200
+    const containerRef = React.useRef<HTMLDivElement | null>(null)
+    const textRef = React.useRef<HTMLDivElement | null>(null)
+    const [fontSize, setFontSize] = React.useState(MAX_FONT_SIZE)
+
+    React.useEffect(() => {
+        const adjustFontSize = () => {
+            if (!containerRef.current || !textRef.current) return
+
+            const containerWidth = containerRef.current.offsetWidth
+            const textWidth = textRef.current.offsetWidth
+
+            if (textWidth > containerWidth) {
+                let newFontSize = MAX_FONT_SIZE
+                while (
+                    textRef.current.offsetWidth > containerWidth &&
+                    newFontSize > 5
+                ) {
+                    newFontSize -= 1
+                    setFontSize(newFontSize)
+                }
+            }
+        }
+
+        const observer = new ResizeObserver(adjustFontSize)
+        if (containerRef.current) {
+            observer.observe(containerRef.current)
+        }
+
+        return () => observer.disconnect()
+    }, [])
+
+    useEffect(() => {
+        if (textRef.current) {
+            textRef.current.style.setProperty(
+                '--calculated-font-size',
+                `${fontSize}px`,
+            )
+        }
+    }, [fontSize])
+
     const formatedSiteName = props.siteName
+        .toLowerCase()
         .replace(/-/g, ' ')
-        .replace(/\b\w/g, (l) => l.toUpperCase())
+        .replace(/^.|\s([a-zæøå])/g, (l) => l.toUpperCase())
+
     return (
         <>
             <div
@@ -19,8 +62,15 @@ export const SiteHeading = (props: {
                     Fotografi
                 </h1>
             </div>
-            <div className="site-heading__horizontal-bar-bottom">
-                <h2 className="site-heading__horizontal-bar-bottom__text type-garamond-regular font-size-extralarge">
+            <div className="site-heading__horizontal-bar-bottom"></div>
+            <div
+                className="site-heading__horizontal-bar-bottom__text-container"
+                ref={containerRef}
+            >
+                <h2
+                    className="site-heading__horizontal-bar-bottom__text-container__text type-garamond-regular font-size-extralarge"
+                    ref={textRef}
+                >
                     {`– ${formatedSiteName}`}
                 </h2>
             </div>
